5c085b3aa8982f300975d59046b7253224d0e0a3
sarafanshul <anshulsaraf3@gmail.com>
2020-06-02 23:48:02 +0530

    2020-6-2 -WHO THE FUCK CAME UP WITH MAKE?

---
+++ main_dir/2020/6/2/11053.py
@@ -0,0 +1,9 @@
+import sys
+a=int(input())
+b=list(map(int, sys.stdin.readline().split()))
+d=[1 for _ in range(a)]
+for i in range(0,a):
+    for j in range(0,i):
+        if (b[j]<b[i]) and (d[i]<d[j]+1):
+            d[i]=d[j]+1
+print(max(d))
