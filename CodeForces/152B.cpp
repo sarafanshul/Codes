5e88d5a5a536ec69bdfd8da4bc7c7936250c1ca7
sarafanshul <anshulsaraf3@gmail.com>
2020-09-16 19:47:37 +0530

    CF

---
+++ CodeForces/152B.cpp
@@ -0,0 +1,75 @@
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define ALL(x) x.begin(),x.end()
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+#define ll long long
+#define double long double
+#define MP make_pair
+// #define int long long
+// #define MAX LONG_LONG_MAX
+// #define MIN LONG_LONG_MIN
+
+using namespace std;
+
+#ifdef CUST_DEBUG // </COMMENT> the {ostream operator} modification(for redifination conflicts) after endif
+template<class K, class V>ostream& operator<<(ostream&s,const pair<K,V>&p){s<<'<'<<p.x<<','<<p.y<<'>';return s;}
+template<class K, class V>ostream& operator<<(ostream&s,const pair<K,V>&p){s<<'<'<<p.F<<','<<p.S<<'>';return s;}
+template<class T, class=typename T::value_type, class=typename enable_if<!is_same<T,string>::value>::type>
+ostream& operator<<(ostream&s,const T&v){s<<'[';for(auto&x:v){s<<x<<", ";}if(!v.empty()){s<<"\b\b";}s<<']';return s;}
+void __prnt(){cerr<<endl;} template<class T, class...Ts>void __prnt(T&&a,Ts&&...etc){cerr<<a<<' ';__prnt(etc...);}
+#define print(...) __prnt(__VA_ARGS__)
+#else
+#define print(...)
+#endif
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p){return cout<<"("<<p.F<<", "<<p.S<<")";}
+template<typename A> ostream& operator<<(ostream &cout,vector<A> const &v){cout<<"[";for(int i=0;i<v.size();i++){if(i)cout<<", ";cout<<v[i];}return cout<<"]";}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p){cin>>p.F;return cin>>p.S;}
+
+const size_t MAXN = 1e5 +7;
+ll n ,m;
+
+template<typename T = long long >
+inline bool onField(T xc ,T yc){
+	if((xc <= n && yc <= m) && (xc > 0 && yc > 0)) return 1;
+	return 0;
+}
+
+void check(){
+	cin >> n >> m;
+	ll xc ,yc ,k ,dx ,dy;
+	cin >> xc >> yc;
+	cin >> k;
+	ll ans = 0;
+	for(int i = 0; i < k; i++){
+		cin >> dx >> dy;
+		for (long long cof = 1100000000; cof; cof /= 2){
+			while (onField(xc + cof * dx, yc + cof * dy)) {
+				xc = xc + cof * dx;
+				yc = yc + cof * dy;
+				ans += cof;
+			}	
+		}
+	}
+	cout << ans ;
+}
+
+int32_t main(){
+	#ifndef CUST_DEBUG
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	#endif
+	// cin.exceptions(cin.Failbit);
+	int t = 1;	
+	// cin >> t;
+	for(int i = 1 ; i <= t ;i++){
+		// cout << "Case "<< i << ":\n";
+		check();
+	}
+	return 0;
+}
