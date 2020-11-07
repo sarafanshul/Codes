cdb7c718f16307c6eb72952987d0d5bfea51d08f
sarafanshul <anshulsaraf3@gmail.com>
2020-11-07 22:19:13 +0530

    CF

---
+++ CodeForces/776C.cpp
@@ -0,0 +1,114 @@
+//https://codeforces.com/blog/entry/50622?#comment-346744
+
+
+#pragma GCC optimize("Ofast")  
+#pragma GCC target("avx,avx2,fma") 
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("unroll-loops")
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+#include <bits/stdc++.h>
+#define ALL(x) x.begin(),x.end()
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+#define ll long long
+#define double long double
+#define MP make_pair
+// #define int long long
+// #define MAX LONG_LONG_MAX
+// #define MIN LONG_LONG_MIN
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#ifdef CUST_DEBUG // </COMMENT> the {ostream operator} modification(for redifination conflicts) after endif
+template<class K, class V>ostream& operator<<(ostream&s,const pair<K,V>&p){s<<'<'<<p.x<<','<<p.y<<'>';return s;}
+template<class K, class V>ostream& operator<<(ostream&s,const pair<K,V>&p){s<<'<'<<p.F<<','<<p.S<<'>';return s;}
+template<class T, class=typename T::value_type, class=typename enable_if<!is_same<T,string>::value>::type>
+ostream& operator<<(ostream&s,const T&v){s<<'[';for(auto&x:v){s<<x<<", ";}if(!v.empty()){s<<"\b\b";}s<<']';return s;}
+void __prnt(){cerr<<endl;} template<class T, class...Ts>void __prnt(T&&a,Ts&&...etc){cerr<<a<<' ';__prnt(etc...);}
+#define print(...) __prnt(__VA_ARGS__)
+#else
+#define print(...)
+#endif
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p){return cout<<"("<<p.F<<", "<<p.S<<")";}
+template<typename A> ostream& operator<<(ostream &cout,vector<A> const &v){cout<<"[";for(int i=0;i<v.size();i++){if(i)cout<<", ";cout<<v[i];}return cout<<"]";}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p){cin>>p.F;return cin>>p.S;}
+
+typedef tree<
+	long long, // Key type
+	null_type, // Mapped-policy
+	less<long long>, // Key comparison functor
+	rb_tree_tag, // Specifies which underlying data structure to use
+	tree_order_statistics_node_update> // A policy for updating node invariants
+ordered_set;
+
+const long long MAXN = 1e5 +7;
+
+
+
+// ============================================================= //
+////  hash for ll <>
+struct custom_hash_ll {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+void check(){
+	ll n ,a[MAXN] ,pre[MAXN] = {0} ,k;
+	cin >> n >> k;
+
+	gp_hash_table<ll ,ll ,custom_hash_ll> mp;
+	mp[1] = 1;
+	ll pw = k;
+	if(k != -1 && k != 1)  for(ll i = 0 ; abs(pw) <= 1e14; i++){ mp[pw] =1 ; pw *= k; }
+	else if(k == -1)  mp[-1] = 1;
+
+	for(ll i = 1; i <= n ;i++) {
+		cin >> a[i];
+		pre[i] = pre[i-1] + a[i];
+	}
+	
+	ll ans = 0;
+	
+	auto get = [&](ll x)->ll{
+		gp_hash_table<ll ,ll ,custom_hash_ll> mp1;
+		ll ret = 0;
+		mp1[0]++;
+		for(ll i = 1 ;i <= n ;i++){
+			ret += mp1[pre[i] - x];
+			mp1[pre[i]]++;
+		}
+		return ret;
+	};
+
+	for(auto val : mp){ ans += get(val.F); }
+	cout << ans<<'\n';
+}
+
+int32_t main(){
+	#ifndef CUST_DEBUG
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	#endif
+	// cin.exceptions(cin.Failbit);
+	int t = 1;	
+	// cin >> t;
+	for(int i = 1 ; i <= t ;i++){
+		// cout << "Case "<< i << ":\n";
+		check();
+	}
+	return 0;
+}
