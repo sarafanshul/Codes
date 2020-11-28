400b3c2000fc21b80f123cd021015b4d46f8be90
sarafanshul <anshulsaraf3@gmail.com>
2020-11-29 00:18:57 +0530

    CF

---
+++ CodeForces/467C.cpp
@@ -0,0 +1,68 @@
+#ifndef CUST_DEBUG
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma") 
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("unroll-loops")
+#endif
+#include <bits/stdc++.h>
+#define ALL(x) x.begin(),x.end()
+#define PB push_back
+#define EB emplace_back
+#define F first
+#define S second
+#define ll long long
+#define double long double
+#define MP make_pair
+// #define int long long
+
+using namespace std;
+
+
+#ifdef CUST_DEBUG
+template<class K, class V>ostream& operator<<(ostream&s,const pair<K,V>&p){s<<'<'<<p.F<<','<<p.S<<'>';return s;}
+template<class T, class=typename T::value_type, class=typename enable_if<!is_same<T,string>::value>::type>
+ostream& operator<<(ostream&s,const T&v){s<<'[';for(auto&x:v){s<<x<<", ";}if(!v.empty()){s<<"\b\b";}s<<']';return s;}
+void __prnt(){cerr<<endl;} template<class T, class...Ts>void __prnt(T&&a,Ts&&...etc){cerr<<a<<' ';__prnt(etc...);}
+#define print(...) __prnt(__VA_ARGS__)
+#else
+#define print(...)
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p){return cout<<"("<<p.F<<", "<<p.S<<")";}
+template<typename A> ostream& operator<<(ostream &cout,vector<A> const &v){cout<<"[";for(int i=0;i<v.size();i++){if(i)cout<<", ";cout<<v[i];}return cout<<"]";}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p){cin>>p.F;return cin>>p.S;}
+#endif
+
+const long long MAXN = 5e3 +7;
+
+ll dp[MAXN][MAXN] = {0};
+ll a[MAXN] ,pre[MAXN] = {0} ;
+
+void check(){
+	ll n ,m , k;
+	cin >> n >> m >> k;
+	for(ll i = 1 ; i <= n ; i++){
+		cin >> a[i];
+		pre[i] = pre[i-1] + a[i];
+	}
+	
+	for(ll i = 1 ; i <= k ; i++){
+		for(ll j = i*m ; j <= n ; j++){
+			dp[i][j] = max( dp[i][j-1] , dp[i-1][j-m] + pre[j] - pre[j-m] );
+		}
+	}
+	cout << dp[k][n]<<'\n';
+}
+
+int32_t main(){
+	#ifndef CUST_DEBUG
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	#endif
+	// cin.exceptions(cin.Failbit);
+	int t = 1;	
+	// cin >> t;
+	for(int i = 1 ; i <= t ;i++){
+		// cout << "Case "<< i << ":\n";
+		check();
+	}
+	return 0;
+}
