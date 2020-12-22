3e13bcca5155a512679983075de4423cc5634d5a
sarafanshul <anshulsaraf3@gmail.com>
2020-12-23 01:09:14 +0530

    CUSTOM | CSES

---
+++ OJs/1084.cc
@@ -0,0 +1,66 @@
+#ifndef CUST_DEBUG
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma") 
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("unroll-loops")
+#endif
+#include <bits/stdc++.h>
+#define ALL(x) x.begin(),x.end()
+#define PB push_back
+#define F first
+#define S second
+#define ll long long
+#define double long double
+#define MP make_pair
+
+using namespace std;
+
+#ifdef CUST_DEBUG
+template<class K, class V>ostream& operator<<(ostream&s,const pair<K,V>&p){s<<'<'<<p.F<<','<<p.S<<'>';return s;}
+template<class T, class=typename T::value_type, class=typename enable_if<!is_same<T,string>::value>::type>
+ostream& operator<<(ostream&s,const T&v){s<<'[';for(auto&x:v){s<<x<<", ";}if(!v.empty()){s<<"\b\b";}s<<']';return s;}
+void __prnt(){cerr<<endl;} template<class T, class...Ts>void __prnt(T&&a,Ts&&...etc){cerr<<a<<' ';__prnt(etc...);}
+#define print(...) __prnt(__VA_ARGS__)
+#else
+#define print(...)
+#endif
+
+const long long MAXN = 1e5 +7;
+
+void check(){
+	multiset<ll> st ;
+	ll n , m, k;
+	cin >> n >> m >> k ;
+	vector<ll> a(n) ;
+	for(ll &i : a){
+		cin >> i ;
+		st.insert( i ) ;
+	}
+	vector<ll> b( m ) ;
+	for(ll &i : b){
+		cin >> i;
+	}
+	sort(ALL(b)) ;
+
+	ll ans = 0 ;
+	
+	for(ll&x : b){
+		auto ub = st.lower_bound(x - k);
+		if(ub != st.end() && *ub <= x + k){
+			ans++;
+			st.erase(ub) ;
+		}
+	}
+	
+	cout << ans <<'\n';
+}
+
+int32_t main(){
+	#ifndef CUST_DEBUG
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	#endif
+	int t = 1;	
+	// cin >> t;
+	for(int i = 1 ; i <= t ;i++){ check(); }
+	return 0;
+}
---
+++ OJs/1091.cc
@@ -0,0 +1,61 @@
+#ifndef CUST_DEBUG
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma") 
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("unroll-loops")
+#endif
+#include <bits/stdc++.h>
+#define ALL(x) x.begin(),x.end()
+#define PB push_back
+#define F first
+#define S second
+#define ll long long
+#define double long double
+#define MP make_pair
+
+using namespace std;
+
+#ifdef CUST_DEBUG
+template<class K, class V>ostream& operator<<(ostream&s,const pair<K,V>&p){s<<'<'<<p.F<<','<<p.S<<'>';return s;}
+template<class T, class=typename T::value_type, class=typename enable_if<!is_same<T,string>::value>::type>
+ostream& operator<<(ostream&s,const T&v){s<<'[';for(auto&x:v){s<<x<<", ";}if(!v.empty()){s<<"\b\b";}s<<']';return s;}
+void __prnt(){cerr<<endl;} template<class T, class...Ts>void __prnt(T&&a,Ts&&...etc){cerr<<a<<' ';__prnt(etc...);}
+#define print(...) __prnt(__VA_ARGS__)
+#else
+#define print(...)
+#endif
+
+const long long MAXN = 1e5 +7;
+
+void check(){
+	ll n, m ;
+	cin >> n >> m;
+	multiset<ll, greater<int>> st;
+
+	ll x ;
+	while (n--) {
+		cin >> x;
+		st.insert(x);
+	}
+
+	ll p;
+	while (m--) {
+		cin >> p;
+		auto it = st.lower_bound(p);
+		if (it == st.end()) { cout << -1 << endl; }
+		else {
+			cout << *it << endl;
+			st.erase(it);
+		}
+	}
+}
+
+int32_t main(){
+	#ifndef CUST_DEBUG
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	#endif
+	int t = 1;	
+	// cin >> t;
+	for(int i = 1 ; i <= t ;i++){ check(); }
+	return 0;
+}
